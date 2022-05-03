1041879a04cc5a73658c46a1a48a3e6225c56f60
Anatolii Iagov <iagov@devexperts.com>
2022-05-03 16:10:58 +0300

    Initial Commit

---
+++ FlickrFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlickrFeed
+//
+//  Created by Anatoliy Yagov on 03.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
